10a4bcad8d85a6dc92f0f54c3cc2d2e5e2c4e58c
Leonardo Ramljak <leonardo.ramljak@gmail.com>
2019-03-05 22:06:12 +0100

    Example: Insert, Update, Delete, Search

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ $dsn = 'mysql:host=' . $host . ';dbname=' . $dbname;
 // Create a PDO instance
 $pdo = new PDO($dsn, $user, $password);
 $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
 # PDO QUERY
 
@@ -35,12 +36,13 @@ $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 $author = 'Brad';
 $is_published = true;
 $id = 1;
+$limit = 1;
 
 // Positional Params
-//$sql = 'SELECT * FROM posts WHERE author = ?';
-//$stmt = $pdo->prepare($sql);
-//$stmt->execute([$author]);
-//$posts = $stmt->fetchAll();
+$sql = 'SELECT * FROM posts WHERE author = ? && is_published = ? LIMIT ?';
+$stmt = $pdo->prepare($sql);
+$stmt->execute([$author, $is_published, $limit]);
+$posts = $stmt->fetchAll();
 
 // Named Params
 //$sql = 'SELECT * FROM posts WHERE author = :author && is_published = :is_published';
@@ -49,18 +51,59 @@ $id = 1;
 //$posts = $stmt->fetchAll();
 
 // var_dump($posts);
+foreach ($posts as $post) {
+    echo $post->title . '<br>';
+}
+
+// FETCH SINGLE POST
+//$sql = 'SELECT * FROM posts WHERE id = :id';
+//$stmt = $pdo->prepare($sql);
+//$stmt->execute(['id' => $id]);
+//$post = $stmt->fetch();
+//
+//echo $post->body;
+
+// GET ROW COUNT
+//$stmt = $pdo->prepare('SELECT * FROM posts WHERE author = ?');
+//$stmt->execute([$author]);
+//$postCount = $stmt->rowCount();
+//
+//echo $postCount;
+
+//  INSERT DATA
+//$title = 'Post Five';
+//$body = 'This is post five';
+//$author = 'Kevin';
+//
+// $sql = 'INSERT INTO posts(title, body, author) VALUES(:title, :body, :author)';
+// $stmt = $pdo->prepare($sql);
+// $stmt->execute(['title' => $title, 'body' => $body, 'author' => $author]);
+// echo 'Post Added';
+
+// UPDATE DATA
+//$id = 1;
+//$body = 'This is the updated post';
+//
+//$sql = 'UPDATE posts SET body = :body WHERE id = :id';
+//$stmt = $pdo->prepare($sql);
+//$stmt->execute(['body' => $body, 'id' => $id]);
+//echo 'Post Updated';
+
+// DELETED DATA
+//$id = 3;
+//
+//$sql = 'DELETE FROM posts WHERE id = :id';
+//$stmt = $pdo->prepare($sql);
+//$stmt->execute(['id' => $id]);
+//echo 'Post Deleted';
+
+// SEARCH DATA
+//$search = "%five%";
+//$sql = 'SELECT * FROM posts WHERE title LIKE ?';
+//$stmt = $pdo->prepare($sql);
+//$stmt->execute([$search]);
+//$posts = $stmt->fetchAll();
+//
 //foreach ($posts as $post) {
 //    echo $post->title . '<br>';
 //}
-
-// FETCH SINGLE POST
-$sql = 'SELECT * FROM posts WHERE id = :id';
-$stmt = $pdo->prepare($sql);
-$stmt->execute(['id' => $id]);
-$post = $stmt->fetch();
-
-?>
-
-<h1><?php echo $post->title ?></h1>
-
-<p><?php echo $post->body ?></p>
